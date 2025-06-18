6077faeac1626eb3376f0ae51315a7d26b9adff7
Girish-120 <98391965+Girish-120@users.noreply.github.com>
2025-06-18 11:06:10 +0530

    changes done

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
  //API_URL: 'http://localhost:3009/',
-  API_URL: 'http://13.235.251.69:5401',
+  API_URL: 'https://apis.flexospaces.com/',
   // API_URL: 'https://apis.flexospaces.com/',
   angularUrl: `http://localhost:4200/`,
   s3_bucket_path: 'worker-app',
